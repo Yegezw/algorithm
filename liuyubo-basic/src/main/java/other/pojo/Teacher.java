1904bbf885f6fbf9ceaf5d19e2c2dc88569e89dc
ZZW <82441492+Yegezw@users.noreply.github.com>
2023-03-12 17:25:33 +0800

    week1-线性查找法 (#1)
    Co-authored-by: zzw <zzw422339@163.com>

---
+++ liuyubo-basic/src/main/java/other/pojo/Teacher.java
@@ -0,0 +1,26 @@
+package other.pojo;
+
+import java.util.Objects;
+
+@SuppressWarnings("all")
+public class Teacher {
+    
+    private String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Teacher another = (Teacher) o;
+        return Objects.equals(name.toLowerCase(), another.name.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Teacher(name = %s)", name);
+    }
+}
