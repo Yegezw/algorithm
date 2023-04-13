25a610b6dd18ca6680c123882c5e63e875c861a2
zzw <zzw422339@163.com>
2023-04-13 19:34:35 +0800

    week13-哈希表、SQRT 分解
    力扣习题
    1、字符串中的第一个唯一字符
    2、区域和检索 - 数组不可变
    3、区域和检索 - 数组可修改

---
+++ liuyubo-basic/src/main/java/other/pojo/Member.java
@@ -0,0 +1,50 @@
+package other.pojo;
+
+import java.util.Objects;
+
+@SuppressWarnings("all")
+public class Member {
+
+    private int grade;
+    private int cls;
+    private String firstName;
+    private String lastName;
+
+    public Member(int grade, int cls, String firstName, String lastName) {
+        this.grade = grade;
+        this.cls = cls;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * 当哈希冲突时, 会调用 equals 判断 2 个对象是否相等
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Member another = (Member) o;
+
+        return grade == another.grade && cls == another.cls &&
+                Objects.equals(firstName.toLowerCase(), another.firstName.toLowerCase()) &&
+                Objects.equals(lastName.toLowerCase(), another.lastName.toLowerCase());
+    }
+
+    /**
+     * 对整型溢出不做处理也是没有问题的, Object 中的 hashCode 根据地址计算
+     */
+    @Override
+    public int hashCode() {
+        int B = 31;
+
+        int hash = 0;
+        hash = hash * B + grade;
+        hash = hash * B + cls;
+        hash = hash * B + (firstName != null ? firstName.toLowerCase().hashCode() : 0);
+        hash = hash * B + (lastName != null ? lastName.toLowerCase().hashCode() : 0);
+
+        return hash;
+    }
+}
---
+++ liuyubo-basic/src/test/java/stage3/week13/HashCodeTest.java
@@ -0,0 +1,49 @@
+package stage3.week13;
+
+import other.pojo.Member;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+@SuppressWarnings("all")
+public class HashCodeTest {
+
+    public static void test() {
+        int a = 42;
+        System.out.println(((Integer) a).hashCode());
+
+        int b = -42;
+        System.out.println(((Integer) b).hashCode());
+
+        double c = 3.1415926;
+        System.out.println(((Double) c).hashCode());
+
+        String d = "imooc";
+        System.out.println(d.hashCode());
+
+        Member member = new Member(3, 2, "bobo", "Liu");
+        System.out.println(member.hashCode());
+    }
+
+    public static void testHashSet() {
+        Member member = new Member(3, 2, "bobo", "Liu");
+
+        HashSet<Member> set = new HashSet<>();
+        set.add(member);
+    }
+
+    public static void testHashMap() {
+        Member stu = new Member(3, 2, "bobo", "Liu");
+
+        HashMap<Member, Integer> scores = new HashMap<>();
+        scores.put(stu, 100);
+    }
+
+    public static void main(String[] args) {
+        test();
+
+        testHashSet();
+
+        testHashMap();
+    }
+}
